ab2005018310c99f4e2b807a49785c39443ee0da
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-05-09 16:10:08 -0700

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = number % 10
+    if number < 0:
+        last_digit = number % -10
+    if last_digit < 0:
+        last_digit = -last_digit
+    print("{}".format(last_digit), end="")
+return last_digit
