a9088b1f34bd3d76f107a81abb1eecf77beeb8b4
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-05-11 15:00:07 -0700

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    reslut = 0
+    for a in range(len(sys.argv) - 1):
+        result = result + int(sys.argv[a + 1])
+        print("{:d}".format(result)
