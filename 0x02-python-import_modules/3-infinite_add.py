ff0f5d9365f69fafe7173b71f80aec10c2671d7a
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-05-11 15:08:09 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    reslut = 0
+    result = 0
     for a in range(len(sys.argv) - 1):
-        result = result + int(sys.argv[a + 1])
-        print("{:d}".format(result))
+        result += int(sys.argv[a + 1])
+        print("{}".format(result))
