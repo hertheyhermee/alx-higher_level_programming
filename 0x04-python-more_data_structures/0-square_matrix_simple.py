7c9f9cb18b2f2ef909299079dbc5734e437e3d7b
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-05-17 04:26:26 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [i if i != search else replace for i in my_list]
+    return [x if x != search else replace for x in my_list]
