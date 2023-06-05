1093e6d19aeb0b2874066db51913c318e14c9d67
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-06-05 03:42:13 -0700

    max integer

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A function that defines text-indentation."""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text: Text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    x = 0
+    while x < len(text) and text[x] == ' ':
+        x += 1
+
+    while x < len(text):
+        print(text[x], end="")
+        if text[x] == "\n" or text[x] in ".?:":
+            if text[x] in ".?:":
+                print("\n")
+            x += 1
+            while x < len(text) and text[x] == ' ':
+                x += 1
+            continue
+        x += 1
