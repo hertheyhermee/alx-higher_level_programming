580427cc4a4bdf540493a30eb0fb1418223dadc8
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-08-16 17:47:45 +0000

    updates

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 
 const [, , arg] = process.argv;
 
@@ -11,7 +10,3 @@ if (!isNaN(checkInt)) {
 } else {
   console.log('Not a number');
 }
-=======
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
->>>>>>> 72e9be9576e5039860a084e7c6d0f46e2872563e
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 // Storing the sentences in an Array
 // then using a for loop to print it
 const lines = [
@@ -10,8 +9,4 @@ const lines = [
 
 for (const line of lines) {
   console.log(line);
-=======
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	  console.log(line);
->>>>>>> 72e9be9576e5039860a084e7c6d0f46e2872563e
 }
